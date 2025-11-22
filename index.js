0e591c4e9ce688a95c845a14732996111ccfe101
erick <ericwambua5150@gmail.com>
2025-11-23 00:54:35 +0300

    modify index file

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-//Write your code here
+//Task 2: Declare and Initialize an Attendee Object.
+
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.0,
+};
+
+//Task 3: Function to log Attendee Name
+
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+//Task 4: Function to log Ticket Price
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+//Task 5: Function to Update Ticket Type
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+//Function to Update Ticket Price
+
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+//Function to remove Event Property
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+//Function to add Checkedin Peoperty
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 
